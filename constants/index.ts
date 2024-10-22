e7582bdc348194cb11615faf33f1c46ad66cd648
KTsybulka <ktsybulka11@gmail.com>
2024-10-22 12:05:32 -0300

    Create NextJS app

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+
+// allow imidualty improt all this thong without nassastery to hardcorndin each bulean points in Nave-item sahered componet
+export const headerLinks = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Create Event",
+    route: "/events/create",
+  },
+  {
+    label: "My Profile",
+    route: "/profile",
+  },
+];
+
+export const eventDefaultValues = {
+  title: "",
+  description: "",
+  location: "",
+  imageUrl: "",
+  startDateTime: new Date(),
+  endDateTime: new Date(),
+  categoryId: "",
+  price: "",
+  isFree: false,
+  url: "",
+};
